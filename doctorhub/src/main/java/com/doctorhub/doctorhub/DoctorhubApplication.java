be838e417558fc325a38268bb663a4d7456530f8
nasaku898 <simonlim898@hotmail.com>
2020-01-26 01:06:15 -0500

    new backend project

---
+++ doctorhub/src/main/java/com/doctorhub/doctorhub/DoctorhubApplication.java
@@ -0,0 +1,13 @@
+package com.doctorhub.doctorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorhubApplication.class, args);
+	}
+
+}
---
+++ doctorhub/src/test/java/com/doctorhub/doctorhub/DoctorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorhub.doctorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
